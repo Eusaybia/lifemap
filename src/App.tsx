455c3de3f8d5d21148f546066243025050c2a2cc
kongweiying <kongwei@eusaybia.com>
2025-12-22 13:47:34 +1100

    feat: Support month+year date formats in TimePointMention
    - Add support for @April 2026, @Apr 26, @April formats
    - Parse full and abbreviated month names
    - Show next occurrence for month-only queries
    - Display '1st Month Year' in dropdown for month timepoints

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import dynamic from 'next/dynamic'
 const AppComponent = dynamic(() => Promise.resolve(() => (
   <React.StrictMode>
     <motion.div style={{ margin: 10, padding: `8px 8px 8px 8px` }}>
-      <Quanta quantaId={'000000'} userId={'000000'} />
+      <Quanta quantaId={'000000'} use8rId={'000000'} />
     </motion.div>
   </React.StrictMode>
 )), {
